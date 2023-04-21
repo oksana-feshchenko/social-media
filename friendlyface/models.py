6cca14f7fee9d5be88ce2b001707bb9978766fec
oksana-feshchenko <foksanv@gmail.com>
2023-04-21 12:48:30 +0300

    Create post ad tag models

--- friendlyface/models.py
+++ friendlyface/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from social_media_api import settings
+
+
+class Tag(models.Model):
+    word = models.CharField(max_length=30)
+
+
+class Post(models.Model):
+    text = models.TextField()
+    tags = models.ManyToManyField(to=Tag, related_name="posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ["created_at"]
+
+    def __str__(self):
+        return self.text[:10] + "..."
+
