150e963624693479196ba3ae30ab28edf4d03d3e
oksana-feshchenko <foksanv@gmail.com>
2023-05-03 17:16:54 +0300

    move django secret key to .env

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&w%7xd7o1!g-1rqjeu9gm-5_w=c@!#f$vns_#m^v8m_vc74((a"
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
